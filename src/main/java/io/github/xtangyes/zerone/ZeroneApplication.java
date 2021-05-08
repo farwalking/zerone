54fad3233da0aea3c9902c7bea139699a5c2fb9e
xtangyes <farwalking@163.com>
2021-05-08 09:13:53 +0800

    初始化项目

---
+++ src/main/java/io/github/xtangyes/zerone/ZeroneApplication.java
@@ -0,0 +1,13 @@
+package io.github.xtangyes.zerone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/xtangyes/zerone/ZeroneApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xtangyes.zerone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
